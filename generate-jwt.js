b4afa5dfd31afbd054d886bc2c56bd8a99dfe92f
David <davidharton14@gmail.com>
2025-07-15 12:50:42 +0300

    remove generate-jwt

--- generate-jwt.js
+++
@@ -1,6 +0,0 @@
-// Script temporar pentru generarea JWT_SECRET
-const crypto = require('crypto');
-const jwtSecret = crypto.randomBytes(64).toString('hex');
-console.log('JWT_SECRET generat:');
-console.log(jwtSecret);
-console.log('\nCopiază această valoare în variabila de mediu JWT_SECRET din Amplify Console.'); 
